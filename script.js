3f6f5b9d250e112538120d16dee393b08eea8e7a
renaate431 <r.goldmane@gmail.com>
2022-03-22 11:14:37 +0200

    otrias

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const vardi = ['Jana Kalnina', 'toms brivmurnieks', 'roberts lengdons', 'luize freska', 'andris dridzis'];
 const balvas = ['phone', 'house', 'ledusskapis', 'kamera', 'TV'];
-const naudaKopa = 100000000000;
+const naudaKopa = 1000000;
 let uzvaretajuSkaits = 5;
-let rindas = document.querySelector('.rindas'); // pievieno mainīgo
-rindas.innerHTML += ' ';
-let balvas = document.querySelector('.balvas'); // pievieno mainīgo
-balvas.innerHTML += ' ';
 
+let rindas = document.querySelector(".rindas"); //pievieno mainīgo
+rindas.innerHTML = ' ';
 
 for (let i = 0; i < uzvaretajuSkaits; i++) {
 
     let rand = Math.random() * vardi.length;
     rand = Math.floor(rand);
-    let uzvaretajs = vardi[rand];
-    let balvas=balvas[rand];
+    let uzvaretaji = vardi[rand];
+    let laimesti = balvas[rand];
+
+
     rindas.innerHTML += `
-    <tr>
-        <td>${i + 1}</td>
-        <td> ${uzvaretajs} </td>
-        <td> ${balvas}</td>
-    </tr>`
+<tr>
+<td>${i + 1}</td>
+<td>${uzvaretaji}</td>
+<td>${laimesti}</td>
+</tr>`
 
 }
